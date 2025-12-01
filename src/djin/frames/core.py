3acf9644ef44f2f4d17c90919219b10af0f7de71
Talbot Knighton <talbotknighton@gmail.com>
2025-11-30 19:35:14 -0800

    updated to parent_ref

--- src/djin/frames/core.py
+++ src/djin/frames/core.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 from contextvars import ContextVar
-from typing import Optional, Self
+from typing import Any, Optional, Self
 import pydantic as pyd
 from typing_extensions import Literal
+from djin.containers.core import Container, Ref, Warehouse
 from djin.math_objects.framed import Pose3D
 from djin.base import immutable
 
@@ -17,35 +18,57 @@ __all__ = [
 class Frame(pyd.BaseModel):
     type_discriminator: Literal["Frame"] = "Frame"
     pose: Pose3D
-    parent: Optional[Frame] = None  # Parent frame
+    parent_ref: Optional[Ref[Frame]] = None  # Parent frame
+
+    def get_parent_optional(
+        self,
+        warehouse: Warehouse[Frame | Any],
+    ) -> Optional[Container[Frame]]:
+        if self.parent_ref is None:
+            return None
+        else:
+            return self.parent_ref.get_optional(warehouse=warehouse)
+
+    def unpack_parent_optional(
+        self, warehouse: Warehouse[Frame | Any]
+    ) -> Optional[Frame]:
+        parent_container = self.get_parent_optional(warehouse=warehouse)
+        if parent_container is None:
+            return None
+        else:
+            return parent_container.contents
 
     def to_parent_frame(
         self,
     ) -> Self:
-        if self.parent is None:
+        parent = self.unpack_parent_optional(warehouse=None)  # TODO real warehouse
+        if parent is None:
             return self.model_copy(deep=True)
         else:
             return type(self)(
                 pose=self.pose.to_parent_frame(starting_frame=self),
-                parent=self.parent.parent,
+                parent_ref=parent.parent_ref,
             )
 
     def to_ground_frame(
         self,
     ) -> Self:
-        if self.parent is None:
+        parent = self.unpack_parent_optional(warehouse=None)  # TODO real warehouse
+        if parent is None:
             return self.model_copy(deep=True)
         else:
             return type(self)(
                 pose=self.pose.to_ground_frame(starting_frame=self),
-                parent=None,
+                parent_ref=None,
             )
 
     def to_target_frame(
         self,
-        target_frame: Frame,
+        target_frame_ref: Ref[Frame],
     ) -> Self:
-        if self.parent is None:
+        parent = self.unpack_parent_optional(warehouse=None)  # TODO real warehouse
+        target_frame = target_frame_ref.unpack(warehouse=None)  # TODO real warehouse
+        if parent is None:
             return self.model_copy(deep=True)
         else:
             return type(self)(
@@ -53,5 +76,5 @@ class Frame(pyd.BaseModel):
                     starting_frame=self,
                     target_frame=target_frame,
                 ),
-                parent=target_frame,
+                parent_ref=target_frame_ref,
             )
